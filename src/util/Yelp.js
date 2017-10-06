d36c0d213e11d1c49b3ab63a8c1f756e3b295162
Paul McKenna <mckennapaul27@gmail.com>
2017-10-06 17:01:56 +0100

    stage4

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const clientId = 'QhcXzGFfAn7V1MsEVK5EFw';
 const secret = 'htdiQzyiDYXk8DMOgx2LdVHKbyZWHmestFrdnv5n6NqoHKFUd4omIJBuaUMHEKKG';
-const accessToken = ;
+const accessToken = getAccessToken();
 
 const Yelp = {
   getAccessToken() {
@@ -17,11 +17,32 @@ const Yelp = {
   },
 
   search(term, location, sortBy) {
-    return Yelp.getAccessToken().then();
-  }
-
-
-
-
+    return Yelp.getAccessToken().then(()=> {
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+        headers: {Authorization: `Bearer ${accessToken}`}
+      });
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if(jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories.title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
+      }
+    })
+  },
 
 };
+
+export default Yelp;
